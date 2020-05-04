bc75134134e9bffd2e41e3b41c587843f88a3b47
andrew28051985 <andrewkrisanov@gmail.com>
2020-05-04 15:34:17 +0300

    make task for gulp  create file min css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
 
-gulp.task('hello', function(done){
-  console.log('привет');
-  done();
+gulp.task('css-min-create', function(cb){
+  return gulp.src('./css/style.css')
+  .pipe(cssnano())
+  .pipe(rename({suffix: '.min'}))
+  .pipe (gulp.dest('./css'));
+  cb();
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('start-gulp', function() {
   browserSync.init({
       server: {
           baseDir: "./"
       }
-  });
+  });  
   gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch('./css/*.css').on("change", browserSync.reload);
+  gulp.watch('./css/*.css').on("change", gulp.parallel('css-min-create'));
 });
