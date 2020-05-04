a1c76a94754a725d57350d3a7ae15fe6b1c3b228
andrew28051985 <andrewkrisanov@gmail.com>
2020-05-04 12:18:00 +0300

    instal BrowserSync & customization gulpfile for browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 
-exports.default = defaultTask
+gulp.task('hello', function(done){
+  console.log('привет');
+  done();
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("./*.html").on("change", browserSync.reload);
+});
