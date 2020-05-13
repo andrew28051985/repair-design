d28b25170a866e1dbb05eb4e4e8de81117286650
andrew28051985 <korotkov915@gmail.com>
2020-05-13 18:39:23 +0300

    feat: modal window ready

--- js/main.js
+++ js/main.js
@@ -5,12 +5,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
+  const clsModalOnClick = document.querySelector('.modal__dialog');
+  
 
   btnModal.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
   btnClose.addEventListener('click', switchModal);
-  console.log(modal);
-  console.log(btnModal);
+  
+  document.onkeydown = function(e) {
+    if (e.keyCode == 27) {
+      modal.classList.remove('modal--visible');
+    }
+  }  
+
+  modal.addEventListener('click', function (e) {
+    if (!clsModalOnClick.contains(e.target) ) {
+      modal.classList.remove('modal--visible');
+    }
+});
+
 });
