500a817f96c52ba0825666cd57e6a89bdb3b0acc
andrew28051985 <korotkov915@gmail.com>
2020-05-15 00:41:04 +0300

    refact: del outline on button__scroll-up & clsModal

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ $(document).ready(function(){
   btnScrolUp.on('click', function() {
     $('body, html').animate({
       scrollTop: 0
-      }, 800);
+      }, 1000);
       return false;
   });
 
