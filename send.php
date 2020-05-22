63133f35f469fccd4a74aa4cfbf1ae5feb56ca09
andrew28051985 <korotkov915@gmail.com>
2020-05-23 02:00:36 +0300

    refact: add send email in send.php with phpmailer

--- send.php
+++ send.php
@@ -3,6 +3,7 @@
 $userName = $_POST['userName'];
 $userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
 
 // Load Composer's autoloader
 require 'phpmailer/PHPMailer.php';
@@ -18,19 +19,19 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'youremail@gmail.com';                     // SMTP username
-    $mail->Password   = 'pass';                               // SMTP password
+    $mail->Username   = 'andrewkrisanov@gmail.com';                     // SMTP username
+    $mail->Password   = 'afrflvbyfv25';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('youremail@gmail.com');
-    $mail->addAddress('client@yandex.ru');     // Add a recipient
+    $mail->setFrom('andrewkrisanov@gmail.com', 'Заявка с сайта');
+    $mail->addAddress('korotkov915@gmail.com');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Новая заявка с сайта';
-    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}. Его вопрос: ${userQuestion}";
 
     $mail->send();
     header('Location: thanks.html');
